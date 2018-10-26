f4e7dc6578ad9172df3d9e107312ee213da948e0
Ibrahim Badamasi <40635278+Ibrobadmos@users.noreply.github.com>
2018-10-26 22:10:10 +0100

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -75,22 +75,6 @@ class DBHelper {
       if (error) {
         callback(error, null);
       } else {
-        // Filter restaurants to have only given neighborhood
-        const results = restaurants.filter(r => r.neighborhood == neighborhood);
-        callback(null, results);
-      }
-    });
-  }
-
-  /**
-   * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
-   */
-  static fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, callback) {
-    // Fetch all restaurants
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
         let results = restaurants
         if (cuisine != 'all') { // filter by cuisine
           results = results.filter(r => r.cuisine_type == cuisine);
